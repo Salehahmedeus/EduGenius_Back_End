8ba614dc8cf025f349c599ef8338489c32307c4c
Salehahmedeus <ahmedfox99999@gmail.com>
2026-01-19 18:47:31 +0200

    feat: Add ProgressReport Eloquent model for tracking user progress.

--- app/Modules/ProgressTracking/Models/ProgressReport.php
+++ app/Modules/ProgressTracking/Models/ProgressReport.php
@@ -6,4 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProgressReport extends Model
 {
+    protected $table = 'progress_reports';
+
+    protected $fillable = [
+        'user_id',
+        'total_quizzes',
+        'average_score',
+        'topics_studied',
+        'strengths',
+        'weaknesses',
+        'generated_at'
+    ];
+
+    protected $casts = [
+        'topics_studied' => 'array',
+        'strengths' => 'array',
+        'weaknesses' => 'array',
+        'generated_at' => 'datetime',
+    ];
 }
