fc6895315f9340c6d4575e23d16b2eb1ad37f952
Salehahmedeus <ahmedfox99999@gmail.com>
2026-01-21 19:36:22 +0200

    Fix route loading: use __DIR__ instead of base_path for Modules

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,13 +3,13 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Support\Facades\Route; 
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        web: __DIR__ . '/../routes/web.php',
         // api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
         then: function () {
             $modules = [
@@ -21,7 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
             ];
 
             foreach ($modules as $module) {
-                $path = base_path("app/Modules/$module/Routes/api.php");
+                $path = __DIR__ . "/../app/Modules/$module/Routes/api.php";
 
                 if (file_exists($path)) {
                     Route::prefix('api')
