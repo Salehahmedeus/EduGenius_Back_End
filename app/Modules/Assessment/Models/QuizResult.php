2d88bfd77fb4c5189c353275f01dc85aa5080687
Salehahmedeus <ahmedfox99999@gmail.com>
2026-01-23 19:31:46 +0200

    update: add casting for attempt_details in QuizResult model

--- app/Modules/Assessment/Models/QuizResult.php
+++ app/Modules/Assessment/Models/QuizResult.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class QuizResult extends Model
 {
-    protected $fillable = ['user_id', 'quiz_id', 'score', 'total_questions', 'correct_answers'];
+    protected $fillable = ['user_id', 'quiz_id', 'score', 'total_questions', 'correct_answers', 'attempt_details'];
+
+    protected $casts = [
+        'attempt_details' => 'array',
+    ];
 }
